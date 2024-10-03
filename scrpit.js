a116b30ca1418daab39b91b4f0766bdee633a8c9
Harshal0Sharma <harshalsharma212@gmail.com>
2024-10-03 12:16:27 +0530

    s

---
+++ scrpit.js
@@ -0,0 +1,19 @@
+document.getElementById("myBtnSuccess").addEventListener("click", function () {
+  alert("Success!");
+});
+
+document.getElementById("myBtnInfo").addEventListener("click", function () {
+  alert("Info!");
+});
+
+document.getElementById("myBtnWarning").addEventListener("click", function () {
+  alert("Warning!");
+});
+
+document.getElementById("myBtnDanger").addEventListener("click", function () {
+  alert("Danger!");
+});
+
+document.getElementById("myBtnDefault").addEventListener("click", function () {
+  alert("Default!");
+});
