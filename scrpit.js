211ab4f4b744184fb6e6f57361029f9c71b03862
Harshal0Sharma <harshalsharma212@gmail.com>
2024-10-03 15:49:24 +0530

    navbar

--- scrpit.js
+++ scrpit.js
@@ -1,19 +1,4 @@
-document.getElementById("myBtnSuccess").addEventListener("click", function () {
-  alert("Success!");
-});
-
-document.getElementById("myBtnInfo").addEventListener("click", function () {
-  alert("Info!");
-});
-
-document.getElementById("myBtnWarning").addEventListener("click", function () {
-  alert("Warning!");
-});
-
-document.getElementById("myBtnDanger").addEventListener("click", function () {
-  alert("Danger!");
-});
-
-document.getElementById("myBtnDefault").addEventListener("click", function () {
-  alert("Default!");
-});
+document.getElementById("menu-btn").onclick = function () {
+  const navbar = document.getElementById("scrollingNavbar");
+  navbar.classList.toggle("active");
+};
